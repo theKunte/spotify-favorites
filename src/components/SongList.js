c90107d689134a347ed4332e53ca40da9a9e71b4
Jenny <jennykunte90@gmail.com>
2025-01-16 17:30:08 -0800

    added components

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,1 +1,19 @@
 //  a list of favorite songs.
+
+import React from "react";
+
+const SongList = ({ songs }) => {
+  return (
+    <ul>
+      {songs.map((song) => (
+        <li>
+          {song.name} by {song.artist} ({Math.floor(song.duration / 60)}:
+          {song.duration % 60})
+        </li>
+      ))}
+      ;
+    </ul>
+  );
+};
+
+export default SongList;
--- src/components/TotalListeningTime.js
+++ src/components/TotalListeningTime.js
@@ -1,1 +1,18 @@
 //Displays the total hours of music listened.
+import React from "react";
+
+const TotalListeningTime = ({ totalTime }) => {
+  const hours = Math.floor({ totalTime } / 3600);
+  const minutes = Math.floor((totalTime % 3600) / 60);
+
+  return (
+    <div className="total-time">
+      <h2>Total Listening Time</h2>
+      <p>
+        {hours} hours and {minutes} minutes
+      </p>
+    </div>
+  );
+};
+
+export default TotalListeningTime;
